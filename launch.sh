c1d76a186d169681fa86f7f8317191917375be2e
Daniel Zeiter <daniel.zeiter@ergon.ch>
2018-01-29 22:11:33 +0100

    Convert dos line endings to unix for private bower scripts
    Fixes upstream issue https://github.com/Hacklone/private-bower/issues/272

--- launch.sh
+++ launch.sh
@@ -2,6 +2,9 @@
 
 chmod -R 600 /root/.ssh
 
+# convert DOS to Unix line endings due to https://github.com/Hacklone/private-bower/issues/272
+sed -i $'s/\r$//' /usr/local/lib/node_modules/private-bower/bin/private-bower
+
 if [ ! -z "$ADDRESS" ]; then
 	sed -i "s/<address>/$ADDRESS/g" /home/private-bower/bowerConfig.json
 fi
